1ee779d31eb3a18348a82de608134cc365d5eed2
Dumie1 <dumiedumara@gmail.com>
2018-09-19 23:10:04 +0200

    updated events code

--- events/index.js
+++ events/index.js
@@ -14,6 +14,33 @@ function add() {
 }
 
 
+
+
+var simon = document.getElementById("simon");
+var bruce = document.getElementById("bruce");
+var ben = document.getElementById("ben");
+
+simon.addEventListener("click", picLink);
+bruce.addEventListener("click", picLink);
+ben.addEventListener("click", picLink);
+
+function picLink() {
+    var allImages = document.querySelectorAll("img");
+
+    for (var i = 0; i < allImages.length; i++) {
+        allImages[i].className = "hide";
+    }
+
+    var picId = this.attributes["data-img"].value;
+    var pic = document.getElementById(picId);
+    if (pic.className === "hide") {
+        pic.className = "";
+    } else {
+        pic.className = "hide";
+    }
+}
+
 /* references below:
 https://www.youtube.com/watch?v=e57ReoUn6kM
+https://www.youtube.com/redirect?event=video_description&v=e57ReoUn6kM&q=https%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2Fdocs%2FWeb%2FEvents&redir_token=-COitRX-nIQAwEz8IVv3PhG-DYJ8MTUzNzQ3MTEzMkAxNTM3Mzg0NzMy
 */
